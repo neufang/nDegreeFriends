a7266d173a26ad897a4bfc802a720fad57fcfcc6
Fang Xu <allxufang@gmail.com>
2013-08-10 20:27:41 +0200

    update the degree of new connected friends
    Given two friends pair 
    "davidbowie  omid   1" 
    "davidbowie  kim   2"
    Degree(omid,kim) 
    = Degree(davidbowie,omid)+Degree(davidbowie,kim)
    = 1 + 2 = 3

--- friends_linker.py
+++ friends_linker.py
@@ -1,28 +1,36 @@
 import sys
-
+from collections import defaultdict
 current_user = None
-connection_set = set()
-
+connection_dict = dict()
+dgr=None
 # input comes from STDIN
 for line in sys.stdin:
     # remove leading and trailing whitespace
     line = line.strip()
     #print out N-degree friend (in the example case N=1)
-    print line
 
     # parse the input we got from mapper.py
-    user, friend = line.split('\t', 1)
+    user, friend,degree = line.split('\t')
+    degree = int(degree)
+    #get number of degree 
+
+    print line
     # this IF-switch only works because Hadoop sorts map output
     # by key (here: word) before it is passed to the reducer
     # two users if they share a identical friend are paired up, so they are considered as (N+1)-degree friends 
     if current_user == user:
 
-        for people in connection_set:
-            print "%s\t%s" %(friend, people)
-            print "%s\t%s" %(people,friend)
+        for people in connection_dict:
+            # set the degree of new pair of N-degree friends (n_i, n_j)
+            # Degree(n_i,n_j)= Degree(n_i, user) + Degree(user, n_j)
+            updated_degree = dgr + connection_dict[people]
+
+            print "%s\t%s\t%d" %(friend, people,updated_degree )
+            print "%s\t%s\t%d" %(people,friend,updated_degree )
 
     else:
+        dgr=degree
         current_user = user
-        connection_set.clear()
+        connection_dict.clear()
 
-    connection_set.add(friend)
+    connection_dict[friend]=degree
