6c486df82ba3b1d5256ff14c21eb7fddb92d9d39
Fang Xu <allxufang@gmail.com>
2013-08-10 20:32:08 +0200

    add up-bound degree N
    output each user’s i-th degree friends, for every positive i less than or equal to N. 
    
    For lager N, multiple run of friends_linked.py is needed.

--- friends_reducer.py
+++ friends_reducer.py
@@ -1,5 +1,7 @@
-#reducer.py
 import sys
+
+#global variable to control the degree of friends
+N=2
 
 current_user = None
 current_set = set()
@@ -10,20 +12,22 @@ for line in sys.stdin:
     line = line.strip()
 
     # parse the input we got from linker.py
-    user, friend = line.split('\t', 1)
+    user, friend,degree = line.split()
+    degree = int(degree)
 
     if current_user == user:
         #add new friend to the current friend-set
-        current_set.add(friend)
+        if degree<=N:
+            current_set.add(friend)
     else:
         if current_user:
             # write list of friends to STDOUT
             print '%s\t%s' % (current_user, "\t".join(sorted(current_set)))
         current_set.clear()
-        current_set.add(friend)
+        if degree <=N:
+            current_set.add(friend)
         current_user = user
 
 # do not forget to output the last word if needed!
 if current_user == user:
     print '%s\t%s' % (current_user, "\t".join(sorted(current_set)))
-	
